50133b99c4634d024358d6cafe653d6f44902cdd
unknown <sivaranjanib99@gmail.com>
2021-08-08 21:58:28 +0530

    [siva] creating the car invoice generator

---
+++ CarInvoiceGeneration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarInvoiceGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to car invoice generator");
+        }
+    }
+}
