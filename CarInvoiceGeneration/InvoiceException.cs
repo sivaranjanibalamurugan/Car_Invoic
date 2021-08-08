58adb420d177084f6b97534429aa27480df511f0
unknown <sivaranjanib99@gmail.com>
2021-08-08 22:07:32 +0530

    [siva] creating invoice for single ride

---
+++ CarInvoiceGeneration/InvoiceException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInvoiceGeneration
+{
+    class InvoiceException : Exception
+    {
+        public ExceptionType exception;
+        public enum ExceptionType
+        {
+            INVALID_DISTANCE, INVALID_TIME
+        }
+        public InvoiceException(ExceptionType exception, string message) : base(message)
+        {
+            this.exception = exception;
+        }
+    
+    }
+}
